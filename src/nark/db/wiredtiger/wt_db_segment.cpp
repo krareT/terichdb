f1285517f607ed4eb49d0b06b92ce922a3093eb5
rockeet <rockeet@163.com>
2016-01-07 23:49:02 +0800

    git add src/nark/db/wiredtiger/wt_db_segment.?pp

---
+++ src/nark/db/wiredtiger/wt_db_segment.cpp
@@ -0,0 +1,129 @@
+#include "wt_db_segment.hpp"
+#include "wt_db_index.hpp"
+#include "wt_db_store.hpp"
+#include "wt_db_context.hpp"
+
+namespace nark { namespace db { namespace wt {
+
+WtWritableSegment::WtWritableSegment() {
+	m_wtConn = NULL;
+	m_wrRowStore = NULL;
+	m_cacheSize = 1*(1ul << 30); // 1GB
+}
+WtWritableSegment::~WtWritableSegment() {
+	m_indices.clear();
+	m_rowStore.reset();
+	if (m_wtConn)
+		m_wtConn->close(m_wtConn, NULL);
+}
+
+void WtWritableSegment::init(PathRef segDir) {
+	std::string strDir = segDir.string();
+	char conf[512];
+	snprintf(conf, sizeof(conf)
+		, "create,cache_size=%zd,"
+		  "log=(enabled,recover=on),"
+		  "checkpoint=(log_size=64MB,wait=60)"
+		, m_cacheSize);
+	int err = wiredtiger_open(strDir.c_str(), NULL, conf, &m_wtConn);
+	if (err) {
+		THROW_STD(invalid_argument, "FATAL: wiredtiger_open(dir=%s,conf=%s) = %s"
+			, strDir.c_str(), conf, wiredtiger_strerror(err)
+			);
+	}
+	WT_SESSION* session = NULL;
+	err = m_wtConn->open_session(m_wtConn, NULL, NULL, &session);
+	if (err) {
+		THROW_STD(invalid_argument, "FATAL: wiredtiger open session(dir=%s) = %s"
+			, strDir.c_str(), wiredtiger_strerror(err)
+			);
+	}
+	m_rowStore = new WtWritableStore(session, segDir);
+	m_wrRowStore = m_rowStore->getWritableStore();
+}
+
+ReadableIndex*
+WtWritableSegment::createIndex(const Schema& schema, PathRef segDir) const {
+	std::string strDir = segDir.string();
+	WT_SESSION* session = NULL;
+	int err = m_wtConn->open_session(m_wtConn, NULL, NULL, &session);
+	if (err) {
+		THROW_STD(invalid_argument, "FATAL: wiredtiger open session(dir=%s) = %s"
+			, strDir.c_str(), wiredtiger_strerror(err)
+			);
+	}
+	return new WtWritableIndex(schema, segDir, session);
+}
+
+ReadableIndex*
+WtWritableSegment::openIndex(const Schema& schema, PathRef segDir) const {
+	std::string strDir = segDir.string();
+	WT_SESSION* session = NULL;
+	int err = m_wtConn->open_session(m_wtConn, NULL, NULL, &session);
+	if (err) {
+		THROW_STD(invalid_argument, "FATAL: wiredtiger open session(dir=%s) = %s"
+			, strDir.c_str(), wiredtiger_strerror(err)
+			);
+	}
+	return new WtWritableIndex(schema, segDir, session);
+}
+
+llong WtWritableSegment::totalStorageSize() const {
+	return m_rowStore->dataStorageSize() + totalIndexSize();
+}
+
+void WtWritableSegment::loadRecordStore(PathRef segDir) {
+	m_rowStore->load(segDir);
+}
+
+void WtWritableSegment::saveRecordStore(PathRef segDir) const {
+	m_rowStore->save(segDir);
+}
+
+llong WtWritableSegment::dataStorageSize() const {
+	return m_rowStore->dataStorageSize();
+}
+
+void WtWritableSegment::getValueAppend(llong id, valvec<byte>* val, DbContext* ctx) const {
+	return m_rowStore->getValueAppend(id, val, ctx);
+}
+
+StoreIterator* WtWritableSegment::createStoreIterForward(DbContext* ctx) const {
+	return m_rowStore->createStoreIterForward(ctx);
+}
+
+StoreIterator* WtWritableSegment::createStoreIterBackward(DbContext* ctx) const {
+	return m_rowStore->createStoreIterBackward(ctx);
+}
+
+llong WtWritableSegment::append(fstring row, DbContext* ctx) {
+	return m_wrRowStore->append(row, ctx);
+}
+
+void WtWritableSegment::replace(llong id, fstring row, DbContext* ctx) {
+	m_wrRowStore->replace(id, row, ctx);
+}
+
+void WtWritableSegment::remove(llong id, DbContext* ctx) {
+	m_wrRowStore->remove(id, ctx);
+}
+
+void WtWritableSegment::clear() {
+	m_wrRowStore->clear();
+}
+
+WritableStore* WtWritableSegment::getWritableStore() {
+	return this;
+}
+
+void WtWritableSegment::save(PathRef path) const {
+	m_wtConn->async_flush(m_wtConn);
+	WritableSegment::save(path);
+}
+
+void WtWritableSegment::load(PathRef path) {
+	init(path);
+	WritableSegment::load(path);
+}
+
+}}} // namespace nark::db::wt
---
+++ src/nark/db/wiredtiger/wt_db_segment.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <nark/db/db_segment.hpp>
+#include <wiredtiger.h>
+#include <tbb/tbb_thread.h>
+
+namespace nark { namespace db { namespace wt {
+
+class NARK_DB_DLL WtWritableSegment : public WritableSegment {
+public:
+	WtWritableSegment();
+	~WtWritableSegment();
+
+	void init(PathRef segDir);
+
+protected:
+	ReadableIndex* createIndex(const Schema&, PathRef path) const override;
+	ReadableIndex* openIndex(const Schema&, PathRef path) const override;
+
+	WT_CONNECTION* m_wtConn;
+	ReadableStorePtr m_rowStore;
+	WritableStore*   m_wrRowStore;
+
+public:
+	llong totalStorageSize() const override;
+	void loadRecordStore(PathRef segDir);
+	void saveRecordStore(PathRef segDir) const;
+
+	llong dataStorageSize() const override;
+	void getValueAppend(llong id, valvec<byte>* val, DbContext*) const override;
+
+	StoreIterator* createStoreIterForward(DbContext*) const override;
+	StoreIterator* createStoreIterBackward(DbContext*) const override;
+
+	llong append(fstring row, DbContext*) override;
+	void  replace(llong id, fstring row, DbContext*) override;
+	void  remove(llong id, DbContext*) override;
+
+	void clear() override;
+
+	WritableStore* getWritableStore() override;
+
+	void save(PathRef path) const override;
+	void load(PathRef path) override;
+
+	size_t m_cacheSize;
+};
+
+}}} // namespace nark::db::wt
