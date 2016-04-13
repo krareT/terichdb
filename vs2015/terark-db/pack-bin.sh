bdfabff320bdbf7383cffbd0bd59f5e0ba38b517
rockeet <rockeet@163.com>
2016-04-13 22:10:26 +0800

    package: Add include dir for windows package

--- vs2015/terark-db/pack-bin.sh
+++ vs2015/terark-db/pack-bin.sh
@@ -2,11 +2,28 @@
 
 set -x
 
-rm -rf terark-db-win-x64
-mkdir -p terark-db-win-x64
+TarBall=terark-db-win-x64
+rm -rf ${TarBall}
+mkdir -p ${TarBall}/bin
+mkdir -p ${TarBall}/lib
+mkdir -p ${TarBall}/include/terark/db
+mkdir -p ${TarBall}/include/terark/io
+mkdir -p ${TarBall}/include/terark/thread
+mkdir -p ${TarBall}/include/terark/util
 
-cp x64/Release/*.dll terark-db-win-x64
-git log -n 1 > terark-db-win-x64/package.version.txt
+cp x64/Release/*.dll                         ${TarBall}/lib
+cp x64/Release/terark-db-schema-compile.exe  ${TarBall}/bin
+cp ../../src/terark/db/db_conf.hpp           ${TarBall}/include/terark/db
+cp ../../src/terark/db/db_context.hpp        ${TarBall}/include/terark/db
+cp ../../src/terark/db/db_index.hpp          ${TarBall}/include/terark/db
+cp ../../src/terark/db/db_store.hpp          ${TarBall}/include/terark/db
+cp ../../src/terark/db/db_segment.hpp        ${TarBall}/include/terark/db
+cp ../../src/terark/db/db_table.hpp          ${TarBall}/include/terark/db
+cp ../../terark-base/src/terark/*.hpp        ${TarBall}/include/terark
+cp ../../terark-base/src/terark/io/*.hpp     ${TarBall}/include/terark/io
+cp ../../terark-base/src/terark/thread/*.hpp ${TarBall}/include/terark/thread
+cp ../../terark-base/src/terark/util/*.hpp   ${TarBall}/include/terark/util
+git log -n 1 > ${TarBall}/package.version.txt
 
-tar cjf terark-db-win-x64.tar.bz2 terark-db-win-x64
-scp terark-db-win-x64.tar.bz2 root@nark.cc:/var/www/html/download
+tar cjf ${TarBall}.tar.bz2 ${TarBall}
+scp ${TarBall}.tar.bz2 root@nark.cc:/var/www/html/download
