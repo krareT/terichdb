ae380fd0b0ed816c96bae90f0ad9825167e9206d
rockeet <rockeet@163.com>
2016-09-16 17:25:21 +0800

    vs2015/terark-db/TestStrIndex/TestStrIndex.cpp: Fix for DataIO_need_bswap workaround

--- vs2015/terark-db/TestStrIndex/TestStrIndex.cpp
+++ vs2015/terark-db/TestStrIndex/TestStrIndex.cpp
@@ -34,6 +34,25 @@ struct E {
 	DATA_IO_LOAD_SAVE(E, &x&d)
 };
 
+struct BS1 {
+	char x, y;
+	DATA_IO_LOAD_SAVE(BS1, &x&y)
+};
+struct BS2 {
+	BS1 x;
+	BS1 y;
+	DATA_IO_LOAD_SAVE(BS2, &x&y)
+};
+struct BS3 {
+	BS2 x;
+	unsigned char y[4];
+	DATA_IO_LOAD_SAVE(BS3, &x&y)
+};
+struct ComplexObj {
+	std::vector<int> x;
+	DATA_IO_LOAD_SAVE(ComplexObj, &x)
+};
+
 int main(int argc, char* argv[])
 {
 	using namespace terark;
@@ -49,10 +68,30 @@ int main(int argc, char* argv[])
 	BOOST_STATIC_ASSERT((DataIO_is_dump<MemReader, D>::value) == true);
 	BOOST_STATIC_ASSERT((DataIO_is_dump<MemReader, E>::value) == false);
 	BOOST_STATIC_ASSERT(DataIO_is_dump_by_sizeof(MemReader, A));
+
+	BOOST_STATIC_ASSERT((DataIO_is_dump<MemReader, BS1>::value) == true);
+	BOOST_STATIC_ASSERT((DataIO_is_dump<MemReader, BS2>::value) == true);
+	BOOST_STATIC_ASSERT((DataIO_is_dump<MemReader, BS3>::value) == true);
+
+	BOOST_STATIC_ASSERT((DataIO_need_bswap<A>::value) == true);
+	BOOST_STATIC_ASSERT((DataIO_need_bswap<B>::value) == true);
+	BOOST_STATIC_ASSERT((DataIO_need_bswap<C>::value) == true);
+	BOOST_STATIC_ASSERT((DataIO_need_bswap<D>::value) == true);
+	BOOST_STATIC_ASSERT((DataIO_need_bswap<E>::value) == true);
+
+	BOOST_STATIC_ASSERT((DataIO_need_bswap<BS1>::value) == false);
+	BOOST_STATIC_ASSERT((DataIO_need_bswap<BS2>::value) == false);
+	BOOST_STATIC_ASSERT((DataIO_need_bswap<BS3>::value) == false);
+
+	BOOST_STATIC_ASSERT((DataIO_need_bswap<ComplexObj>::value) == true);
+	BOOST_STATIC_ASSERT((DataIO_is_dump<MemReader, ComplexObj>::value) == false);
+
 //	printf("sizeof(A+B+C) = %zd\n", sizeof(A) + sizeof(B) + sizeof(C));
 //	printf("sizeof(D    ) = %zd\n", sizeof(D));
 	MemWriter wr;
 	wr << D();
+	A a;
+//	a._M_Deduce_DataIO_need_bswap().need_bswap();
     return 0;
 }
 
