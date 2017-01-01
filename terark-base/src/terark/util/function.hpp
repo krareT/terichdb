1023da66a6693abe700a6481e9ce1365c5870896
rockeet <rockeet@gmail.com>
2017-01-01 21:36:09 +0800

    sync private repo

---
+++ terark-base/src/terark/util/function.hpp
@@ -0,0 +1,24 @@
+﻿/* vim: set tabstop=4 : */
+#pragma once
+
+#ifdef TERARK_FUNCTION_USE_BOOST
+	#include <boost/function.hpp>
+#else
+	#include <functional>
+#endif
+
+namespace terark {
+
+#ifdef TERARK_FUNCTION_USE_BOOST
+	using boost::function;
+	using boost::ref;
+	using boost::cref;
+	using boost::reference_wrapper
+#else
+	using std::function;
+	using std::ref;
+	using std::cref;
+	using std::reference_wrapper;
+#endif
+
+} // namespace terark
